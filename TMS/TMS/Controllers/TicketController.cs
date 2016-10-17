bce8e9636fc1f8b1e2cc33a5ec29fad1d5ac734d
bvloi94 <bvloi94@gmail.com>
2016-10-17 12:32:15 +0700

    Layout

--- TMS/TMS/Controllers/TicketController.cs
+++ TMS/TMS/Controllers/TicketController.cs
@@ -12,6 +12,7 @@ using TMS.DAL;
 using TMS.Enumerator;
 using TMS.Models;
 using TMS.Services;
+using TMS.Utils;
 using TMS.ViewModels;
 
 namespace TMS.Controllers
@@ -19,15 +20,18 @@ namespace TMS.Controllers
     public class TicketController : Controller
     {
         private TMSEntities db = new TMSEntities();
-        private UnitOfWork _unitOfWork;
-        private TicketService _ticketService;
-        private TicketAttachmentService _ticketAttachmentService;
+        public TicketService _ticketService { get; set; }
+        public UserService _userService { get; set; }
+        public DepartmentService _departmentService { get; set; }
 
         public TicketController()
         {
-            _unitOfWork = new UnitOfWork();
-            _ticketService = new TicketService(_unitOfWork);
+            var unitOfWork = new UnitOfWork();
+            _ticketService = new TicketService(unitOfWork);
+            _userService = new UserService(unitOfWork);
+            _departmentService = new DepartmentService(unitOfWork);
         }
+
         // GET: Tickets
         public ActionResult Index()
         {
@@ -74,7 +78,7 @@ namespace TMS.Controllers
                 ticket.CreatedTime = DateTime.Now;
                 db.Tickets.Add(ticket);
                 db.SaveChanges();
-                
+
                 return RedirectToAction("Index");
             }
             return View(model);
@@ -155,6 +159,208 @@ namespace TMS.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public ActionResult GetTicketDetail(int id)
+        {
+            Ticket ticket = _ticketService.GetTicketByID(id);
+            AspNetUser solver = _userService.GetUserById(ticket.SolveID);
+            AspNetUser creater = _userService.GetUserById(ticket.CreatedID);
+            AspNetUser assigner = _userService.GetUserById(ticket.AssignedByID);
+            String ticketType, ticketMode;
+
+            switch (ticket.Type)
+            {
+                case 1: ticketType = ConstantUtil.TicketTypeString.Request; break;
+                case 2: ticketType = ConstantUtil.TicketTypeString.Problem; break;
+                case 3: ticketType = ConstantUtil.TicketTypeString.Change; break;
+                default: ticketType = "None"; break;
+            }
+
+            switch (ticket.Mode)
+            {
+                case 1: ticketMode = ConstantUtil.TicketModeString.PhoneCall; break;
+                case 2: ticketMode = ConstantUtil.TicketModeString.WebForm; break;
+                case 3: ticketMode = ConstantUtil.TicketModeString.Email; break;
+                default: ticketMode = "None"; break;
+            }
+
+            return Json(new
+            {
+                id = ticket.ID,
+                subject = ticket.Subject,
+                description = ticket.Description,
+                type = ticketType,
+                mode = ticketMode,
+                urgency = ticket.Urgency == null ? "None" : ticket.Urgency.Name,
+                priority = ticket.Priority == null ? "None" : ticket.Priority.Name,
+                category = ticket.Category == null ? "None" : ticket.Category.Name,
+                impact = ticket.Impact == null ? "None" : ticket.Impact.Name,
+                impactDetail = ticket.ImpactDetail == null ? "None" : ticket.ImpactDetail,
+                status = ticket.Status,
+                createdDate = ticket.CreatedTime.ToString(),
+                lastModified = ticket.ModifiedTime.ToString(),
+                scheduleStart = ticket.ScheduleStartDate.ToString(),
+                scheduleEnd = ticket.ScheduleEndDate.ToString(),
+                actualStart = ticket.ActualStartDate.ToString(),
+                actualEnd = ticket.ActualEndDate.ToString(),
+                solution = ticket.Solution == null ? "None" : ticket.Solution,
+                solver = solver == null ? "None" : solver.Fullname,
+                creater = creater == null ? "None" : creater.Fullname,
+                assigner = assigner == null ? "None" : assigner.Fullname,
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Solve(int id)
+        {
+            Ticket ticket = _ticketService.GetTicketByID(id);
+            if (ticket.Status != ConstantUtil.TicketStatus.Assigned) // Ticket status is not "Assigned"
+            {
+                return RedirectToAction("Index"); // Redirect to Index so the Technician cannot go to Solve view.
+            }
+            else
+            {
+                // Get Ticket information
+                AspNetUser solvedUser = _userService.GetUserById(ticket.SolveID);
+                AspNetUser createdUser = _userService.GetUserById(ticket.CreatedID);
+                AspNetUser assigner = _userService.GetUserById(ticket.AssignedByID);
+                TicketSolveViewModel model = new TicketSolveViewModel();
+
+                model.ID = ticket.ID;
+                model.Subject = ticket.Subject;
+                model.Description = ticket.Description;
+
+                switch (ticket.Mode)
+                {
+                    case 1: model.Mode = ConstantUtil.TicketModeString.PhoneCall; break;
+                    case 2: model.Mode = ConstantUtil.TicketModeString.WebForm; break;
+                    case 3: model.Mode = ConstantUtil.TicketModeString.Email; break;
+                }
+
+                switch (ticket.Type)
+                {
+                    case 1: model.Type = ConstantUtil.TicketTypeString.Request; break;
+                    case 2: model.Type = ConstantUtil.TicketTypeString.Problem; break;
+                    case 3: model.Type = ConstantUtil.TicketTypeString.Change; break;
+                }
+
+                switch (ticket.Status)
+                {
+                    case 1: model.Status = "New"; break;
+                    case 2: model.Status = "Assigned"; break;
+                    case 3: model.Status = "Solved"; break;
+                    case 4: model.Status = "Unapproved"; break;
+                    case 5: model.Status = "Cancelled"; break;
+                    case 6: model.Status = "Closed"; break;
+                }
+
+                model.Category = (ticket.Category == null) ? "None" : ticket.Category.Name;
+                model.Impact = (ticket.Impact == null) ? "None" : ticket.Impact.Name;
+                model.ImpactDetail = (ticket.ImpactDetail == null) ? "None" : ticket.ImpactDetail;
+                model.Urgency = (ticket.Urgency == null) ? "None" : ticket.Urgency.Name;
+                model.Priority = (ticket.Priority == null) ? "None" : ticket.Priority.Name;
+                model.CreateTime = ticket.CreatedTime;
+                model.ModifiedTime = ticket.ModifiedTime;
+                model.ScheduleEndTime = ticket.ScheduleEndDate;
+                model.ScheduleStartTime = ticket.ScheduleStartDate;
+                model.ActualStartTime = ticket.ActualStartDate;
+                model.ActualEndTime = ticket.ActualEndDate;
+                model.CreatedBy = (createdUser == null) ? "None" : createdUser.Fullname;
+                model.AssignedBy = (createdUser == null) ? "None" : assigner.Fullname;
+                model.SolvedBy = (solvedUser == null) ? "None" : solvedUser.Fullname;
+                model.Solution = ticket.Solution;
+                model.UnapproveReason = (string.IsNullOrEmpty(ticket.UnapproveReason)) ? "None" : ticket.UnapproveReason;
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Solve(int id, TicketSolveViewModel model, string command)
+        {
+            Ticket ticket = _ticketService.GetTicketByID(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            switch (command)
+            {
+                case "Solve":
+                    if (ModelState.IsValid)
+                    {
+                        ticket.SolveID = User.Identity.GetUserId();
+                        ticket.SolvedDate = DateTime.Now;
+                        ticket.ModifiedTime = DateTime.Now;
+                        ticket.Solution = model.Solution;
+                        _ticketService.SolveTicket(ticket);
+                        return RedirectToAction("Index");
+                    }
+                    break;
+                case "Save":
+                    if (ModelState.IsValid)
+                    {
+                        ticket.ModifiedTime = DateTime.Now;
+                        ticket.Solution = model.Solution;
+                        _ticketService.UpdateTicket(ticket);
+                        return RedirectToAction("Index");
+                    }
+                    break;
+            }
+
+            // Get Ticket information
+            AspNetUser solvedUser = _userService.GetUserById(ticket.SolveID);
+            AspNetUser createdUser = _userService.GetUserById(ticket.CreatedID);
+            AspNetUser assigner = _userService.GetUserById(ticket.AssignedByID);
+
+            model.ID = ticket.ID;
+            model.Subject = ticket.Subject;
+            model.Description = ticket.Description;
+
+            switch (ticket.Mode)
+            {
+                case 1: model.Mode = ConstantUtil.TicketModeString.PhoneCall; break;
+                case 2: model.Mode = ConstantUtil.TicketModeString.WebForm; break;
+                case 3: model.Mode = ConstantUtil.TicketModeString.Email; break;
+            }
+
+            switch (ticket.Type)
+            {
+                case 1: model.Type = ConstantUtil.TicketTypeString.Request; break;
+                case 2: model.Type = ConstantUtil.TicketTypeString.Problem; break;
+                case 3: model.Type = ConstantUtil.TicketTypeString.Change; break;
+            }
+
+            switch (ticket.Status)
+            {
+                case 1: model.Status = "New"; break;
+                case 2: model.Status = "Assigned"; break;
+                case 3: model.Status = "Solved"; break;
+                case 4: model.Status = "Unapproved"; break;
+                case 5: model.Status = "Cancelled"; break;
+                case 6: model.Status = "Closed"; break;
+            }
+
+            model.Category = (ticket.Category == null) ? "None" : ticket.Category.Name;
+            model.Impact = (ticket.Impact == null) ? "None" : ticket.Impact.Name;
+            model.ImpactDetail = (ticket.ImpactDetail == null) ? "None" : ticket.ImpactDetail;
+            model.Urgency = (ticket.Urgency == null) ? "None" : ticket.Urgency.Name;
+            model.Priority = (ticket.Priority == null) ? "None" : ticket.Priority.Name;
+            model.CreateTime = ticket.CreatedTime;
+            model.ModifiedTime = ticket.ModifiedTime;
+            model.ScheduleEndTime = ticket.ScheduleEndDate;
+            model.ScheduleStartTime = ticket.ScheduleStartDate;
+            model.ActualStartTime = ticket.ActualStartDate;
+            model.ActualEndTime = ticket.ActualEndDate;
+            model.CreatedBy = (createdUser == null) ? "None" : createdUser.Fullname;
+            model.AssignedBy = (createdUser == null) ? "None" : assigner.Fullname;
+            model.SolvedBy = (solvedUser == null) ? "None" : solvedUser.Fullname;
+            model.UnapproveReason = (string.IsNullOrEmpty(ticket.UnapproveReason)) ? "None" : ticket.UnapproveReason;
+
+            return View(model);
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -168,7 +374,7 @@ namespace TMS.Controllers
         public ActionResult GetTickets(jQueryDataTableParamModel param)
         {
             var ticketList = _ticketService.GetAll();
-            
+
             IEnumerable<Ticket> filteredListItems;
             if (!string.IsNullOrEmpty(param.sSearch))
             {
@@ -209,6 +415,6 @@ namespace TMS.Controllers
                 aaData = result
             }, JsonRequestBehavior.AllowGet);
         }
-        
+
     }
 }
