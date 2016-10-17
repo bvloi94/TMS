85c5e971496e5fc43d9952fb4b6ef913f4c2817b
bvloi94 <bvloi94@gmail.com>
2016-10-17 23:57:17 +0700

    cmt

--- TMS/TMS/Controllers/TicketController.cs
+++ TMS/TMS/Controllers/TicketController.cs
@@ -7,6 +7,7 @@ using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Web.ApplicationServices;
 using System.Web.Mvc;
 using TMS.DAL;
 using TMS.Enumerator;
@@ -88,7 +89,7 @@ namespace TMS.Controllers
                     _ticketAttachmentService.saveFile(ticket.ID, uploadFiles);
                     List<TicketAttachment> listFile = unitOfWork.TicketAttachmentRepository.Get(i => i.TicketID == ticket.ID).ToList();
                     ticketFiles.Path = listFile[0].Path;
-                    
+
                 }
 
 
@@ -381,11 +382,12 @@ namespace TMS.Controllers
             Ticket ticket = _ticketService.GetTicketByID(id);
 
             string createdId = "";
-            AspNetUser user;
+            AspNetUser user = null;
             if (User.Identity.GetUserId() != null)
             {
                 user = _userService.GetUserById(createdId);
                 createdId = User.Identity.GetUserId();
+                user.AspNetRoles.ToString();
             }
 
             if (ticket == null)
